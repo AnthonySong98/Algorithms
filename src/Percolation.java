ab0ee1f2bac981f61795d65969cdd99eb4ad90a0
Anthony Song <songzixing98@gmail.com>
2018-08-04 23:52:43 +0800

    Add HW_1

---
+++ src/Percolation.java
@@ -0,0 +1,78 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class Percolation {
+    private WeightedQuickUnionUF weightedQuickUnionUF;
+    private int grid_size;
+    private boolean [][]grid;
+    private int num_of_open_sites;
+    public Percolation(int n)           // create n-by-n grid, with all sites blocked
+    {
+        grid_size = n;
+        weightedQuickUnionUF = new WeightedQuickUnionUF(n*n+2);
+        grid = new boolean[n+1][n+1];
+        num_of_open_sites = 0;
+    }
+    public boolean validIndices(int row, int col){
+        if(row>=1&&row<=grid_size&&col>=1&&col<=grid_size) return true;
+        else return false;
+    }
+    public    void open(int row, int col)    // open site (row, col) if it is not open already
+    {
+        if(grid[row][col]==false&&validIndices(row,col)) {
+            grid[row][col]=true;
+            num_of_open_sites++;
+            {
+                if(row>1&&isOpen(row-1,col)) weightedQuickUnionUF.union(grid_size*(row-1-1)+col,grid_size*(row-1)+col);
+                if(col>1&&isOpen(row,col-1)) weightedQuickUnionUF.union(grid_size*(row-1)+col-1,grid_size*(row-1)+col);
+                if(col<grid_size&&isOpen(row,col+1)) weightedQuickUnionUF.union(grid_size*(row-1)+col+1,grid_size*(row-1)+col);
+                if(row<grid_size&&isOpen(row+1,col)) weightedQuickUnionUF.union(grid_size*(row+1-1)+col,grid_size*(row-1)+col);
+            }
+            if(row==1) weightedQuickUnionUF.union(0,grid_size*(row-1)+col);
+            if(row==grid_size) weightedQuickUnionUF.union(grid_size*grid_size+1,grid_size*(row-1)+col);
+        }
+    }
+    public boolean isOpen(int row, int col)  // is site (row, col) open?
+    {
+        return grid[row][col]==true?true:false;
+    }
+    public boolean isFull(int row, int col)  // is site (row, col) full?
+    {
+        return grid[row][col]==true&&weightedQuickUnionUF.connected(0,grid_size*(row-1)+col)?true:false;
+    }
+    public     int numberOfOpenSites()       // number of open sites
+    {
+       return num_of_open_sites;
+    }
+    public boolean percolates()              // does the system percolate?
+    {
+        if(weightedQuickUnionUF.connected(0,grid_size*grid_size+1)) return true;
+        else return false;
+    }
+
+    public static void main(String[] args)   // test client (optional)
+    {
+        try{
+            FileInputStream input = new FileInputStream("/Users/mac/Desktop/Algorithms/percolation/greeting57.txt");
+            System.setIn(input);
+        }
+        catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+        int n = StdIn.readInt();
+        Percolation percolation = new Percolation(n);
+        while (!StdIn.isEmpty()){
+            int i = StdIn.readInt();
+            int j = StdIn.readInt();
+            percolation.open(i,j);
+        }
+        System.out.println(percolation.percolates());
+        System.out.println(percolation.numberOfOpenSites());
+
+    }
+}
--- src/test/helloworld.java
+++ src/test/helloworld.java
@@ -1,5 +1,7 @@
 package test;
-
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 public class helloworld {
     public static void main(String[]args){
         System.out.println("helloworld");
